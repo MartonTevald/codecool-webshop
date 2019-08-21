fbcfdc714c7189c1568f8f6b9beedd8bf663cc90
Márton Tévald <marton.tevald@gmail.com>
2019-08-21 15:49:32 +0200

    empty test files created

---
+++ tests/com/codecool/shop/dao/implementation/ProductCategoryDaoMemTest.java
@@ -0,0 +1,28 @@
+package com.codecool.shop.dao.implementation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductCategoryDaoMemTest {
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find1() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
---
+++ tests/com/codecool/shop/dao/implementation/ProductDaoMemTest.java
@@ -0,0 +1,32 @@
+package com.codecool.shop.dao.implementation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductDaoMemTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getBy() {
+    }
+
+    @Test
+    void getBy1() {
+    }
+}
