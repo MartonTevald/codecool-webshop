bc1be08d8fdfc55c9ba5b7a1d21664149a7585f5
Roy91 <rolonew@gmail.com>
2019-08-22 14:47:30 +0200

    testedcaTdaoJdbc

---
+++ test_table.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS product, supplier, prodCat;
+
+CREATE TABLE supplier(
+    id  SERIAL PRIMARY KEY ,
+    name VARCHAR(50) UNIQUE ,
+    department varchar(50),
+    description varchar(200));
+
+CREATE TABLE prodCat(
+    id int UNIQUE ,
+    name VARCHAR(50) UNIQUE ,
+    department varchar(50),
+    description varchar(500));
+
+CREATE TABLE product(
+    id SERIAL PRIMARY KEY,
+    name varchar(150) UNIQUE ,
+    description varchar(500),
+    price float,
+    supplier_id int  REFERENCES supplier(id),
+    prodCat_id int REFERENCES prodCat(id));
+
+
+-- CREATE TABLE invoice(
+--     id SERIAL PRIMARY KEY,
+--     order_id int REFERENCES order(id)
+-- );
+--
+-- CREATE TABLE order(
+--     id SERIAL PRIMARY KEY ,
+--     product_id int REFERENCES product(id),
+--     count int,
+--     user_id int REFERENCES user_information(id),
+--     ord_number int,
+--     closed boolean
+--
+-- );
