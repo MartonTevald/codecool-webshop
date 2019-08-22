bdeb0461a251afe59ca82b632c1b2f5fdd353ee4
Roy91 <rolonew@gmail.com>
2019-08-22 10:56:10 +0200

    progres

--- tests/com/codecool/shop/dao/implementation/ProductCategoryDaoJdbcTest.java
+++ tests/com/codecool/shop/dao/implementation/ProductCategoryDaoJdbcTest.java
@@ -1,13 +1,29 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.dao.DatabaseConnection;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.model.ProductCategory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ProductCategoryDaoJdbcTest {
+class ProductCategoryDaoJdbcTest extends DatabaseConnection {
+
+    private ProductCategoryDao productCategoryDataStore = new ProductCategoryDaoJdbc();
+
+    ProductCategory cat1 = new ProductCategory(1,"Tablet","Hardware","very nice");
+    ProductCategory cat2 = new ProductCategory(2,"Laptop","Hardware","very nice");
 
     @Test
     void add() {
+        productCategoryDataStore.add(cat1);
+        productCategoryDataStore.getAll();
+        assertEquals(1,productCategoryDataStore.getAll().size());
     }
 
     @Test
