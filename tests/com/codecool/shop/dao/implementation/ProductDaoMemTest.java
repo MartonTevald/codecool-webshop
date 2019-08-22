e5d399ccde682b14c259d8745565fc4942e5d87e
Márton Tévald <marton.tevald@gmail.com>
2019-08-22 11:02:50 +0200

    product tests done

--- tests/com/codecool/shop/dao/implementation/ProductDaoMemTest.java
+++ tests/com/codecool/shop/dao/implementation/ProductDaoMemTest.java
@@ -1,43 +1,72 @@
 package com.codecool.shop.dao.implementation;
 
-import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductDaoMemTest {
 
-    private ProductDaoMem prod = ProductDaoMem.getInstance();
+    private ProductDaoMem prod;
+
+    @BeforeEach
+    void setup(){
+        prod = ProductDaoMem.getInstance();
+        prod.getAll().clear();
+    }
+
+    ProductCategory cat1 = new ProductCategory(1, "Tablet", "Hardware", "very nice");
+    ProductCategory cat2 = new ProductCategory(2, "Laptop", "Hardware", "very nice");
+    Supplier sup1 = new Supplier(1, "sup1", "supDesc1");
+    Supplier sup2 = new Supplier(2, "sup2", "supDesc2");
+    Product prod1 = new Product(1, "name1", 1, "ISK", "desc1", cat1, sup1);
+    Product prod2 = new Product(2, "name2", 2, "ISK", "desc2", cat2, sup2);
+
 
 
     @Test
-    void add(Product product) {
-
+    void testAddSingleProduct() {
+        prod.add(prod1);
+        assertEquals(1, prod.getAll().size());
     }
 
     @Test
-    void find(int id) {
+    void testFindSingleProduct() {
+        prod.add(prod1);
+        assertEquals(prod1, prod.find(1));
     }
 
     @Test
-    void remove(int id) {
+    void testRemoveSingleProduct() {
+        prod.add(prod1);
+        prod.add(prod2);
+        prod.remove(1);
+        assertEquals(1, prod.getAll().size());
     }
 
     @Test
-    void getAll() {
+    void testGetAllProducts() {
+        prod.add(prod1);
+        prod.add(prod2);
+        assertEquals(2, prod.getAll().size());
     }
 
     @Test
-    void getBySupplier(Supplier supplier) {
+    void testGetBySupplier() {
+        prod.add(prod1);
+        prod.add(prod2);
+        assertEquals(Arrays.asList(prod1), prod.getBy(sup1));
     }
 
     @Test
-    void getByProductCategory(ProductCategory productCategory) {
+    void testGetByProductCategory() {
+        prod.add(prod1);
+        prod.add(prod2);
+        assertEquals(Arrays.asList(prod1), prod.getBy(cat1));
     }
 }
