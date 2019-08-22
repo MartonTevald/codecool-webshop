9e34c6f5d42887f8f4369691bfa573ce97a08fce
Roy91 <rolonew@gmail.com>
2019-08-22 10:03:59 +0200

    catTestedDone

--- tests/com/codecool/shop/dao/implementation/ProductCategoryDaoMemTest.java
+++ tests/com/codecool/shop/dao/implementation/ProductCategoryDaoMemTest.java
@@ -1,28 +1,51 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.model.ProductCategory;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductCategoryDaoMemTest {
 
+    private ProductCategoryDaoMem prodCat = ProductCategoryDaoMem.getInstance();
+
+    ProductCategory cat1 = new ProductCategory(1,"Tablet","Hardware","very nice");
+    ProductCategory cat2 = new ProductCategory(2,"Laptop","Hardware","very nice");
+
+
     @Test
     void add() {
+        prodCat.add(cat1);
+        assertEquals(1, prodCat.getAll().size());
     }
 
     @Test
-    void find() {
+    void findCatById() {
+        prodCat.add(cat1);
+        prodCat.add(cat2);
+        assertEquals(cat1,prodCat.find(1));
     }
 
     @Test
-    void find1() {
+    void findCatByName() {
+        prodCat.add(cat1);
+        assertEquals(cat1,prodCat.find("Tablet"));
     }
 
     @Test
     void remove() {
+        prodCat.add(cat1);
+        prodCat.remove(1);
+        assertEquals(0,prodCat.getAll().size());
     }
 
     @Test
     void getAll() {
+        prodCat.add(cat1);
+        prodCat.add(cat2);
+        assertEquals(2,prodCat.getAll().size());
     }
 }
